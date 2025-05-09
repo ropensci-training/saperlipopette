a8d677878c4d2c6158002702bcc54b19c757fdaf
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-05-09 10:03:37 +0200

    chore: typo fix

--- inst/exo_undo_commit-Rprofile.R
+++ inst/exo_undo_commit-Rprofile.R
@@ -3,7 +3,7 @@ if (file.exists("~/.Rprofile")) {
 }
 
 cli::cli_alert_danger('"Oh shit, I need to undo a commit from like 5 commits ago!"')
-cli::cli_alert_danger("I to undo my 'fix: fix things' commit as it broke stuff!")
+cli::cli_alert_danger("I want to undo my 'fix: fix things' commit as it broke stuff!")
 cli::cli_alert_info("See {.url https://ohshitgit.com/#undo-a-commit}")
 cli::cli_alert_info("For more help use {.run tip()}")
 
