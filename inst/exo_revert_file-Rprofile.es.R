bd258c11d56804102d3c7cdbfb99656daba72471
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-10-24 09:44:17 +0200

    fix: typo fix

--- inst/exo_revert_file-Rprofile.es.R
+++ inst/exo_revert_file-Rprofile.es.R
@@ -3,7 +3,7 @@ if (file.exists("~/.Rprofile")) {
 }
 
 cli::cli_alert_danger(
-  '"Mierda, necesito deshacer un commit que hice hace 5 comits!"'
+  '"Mierda, necesito deshacer un commit que hice hace 5 commits!"'
 )
 cli::cli_alert_danger(
   "Quiero que bla.txt sea como era antes del commit 'fix: repara cosas' !"
