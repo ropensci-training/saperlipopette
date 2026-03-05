9d94af9994a51e351cdd484353b74195b6e9739e
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-03-05 14:28:13 +0100

    test: fix code for interactive testing within RStudio/Positron

--- R/utils-usethis.R
+++ R/utils-usethis.R
@@ -1,6 +1,15 @@
 create_project <- function(path) {
-  rlang::with_options(
-    usethis.quiet = getOption("usethis.quiet") %||% FALSE,
-    usethis::create_project(path)
-  )
+  # do not create rprojfile when testing
+  # testthat:::is_testing()
+  if (identical(Sys.getenv("TESTTHAT"), "true")) {
+    rlang::with_options(
+      usethis.quiet = getOption("usethis.quiet") %||% FALSE,
+      usethis::create_project(path, rstudio = FALSE)
+    )
+  } else {
+    rlang::with_options(
+      usethis.quiet = getOption("usethis.quiet") %||% FALSE,
+      usethis::create_project(path)
+    )
+  }
 }
